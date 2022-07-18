535aa515874fb351f6644c03b77d130badf3edf3
mars1448 <lekeeseschool@gmail.com>
2022-07-18 11:27:25 -0700

    Updated UI

---
+++ introjs/app.js
@@ -0,0 +1,11 @@
+// const string = 'The revolution will not be televised'
+// console.log(string)
+
+
+// const greet = () => {
+//   const name = prompt('Whats yo name cuh')
+//   alert(`Hello ${name}, what poppin`)
+// }
+// const name = 'front'
+// const number = 242
+// console.log(`${name} ${number}`)
